94a454f85ebd856ebc527ffe685db9820618d363
BrianChanLEE <brianlee1914@me.com>
2024-02-07 10:43:52 +0900

    chagend?

--- app/api-doc/page.tsx
+++ app/api-doc/page.tsx
@@ -1,4 +1,4 @@
-import {getApiDocs} from '../swagger';
+import {getApiDocs} from '../../src/lib/swagger';
 import ReactSwagger from './react-swagger';
 
 export default async function IndexPage() {
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,42 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// // /** @type {import('next').NextConfig} */
+// // import {createRequire} from 'module';
+// // const require = createRequire(import.meta.url);
+// // const nextSwaggerDoc = require('next-swagger-doc');
 
-export default nextConfig;
+// // const swaggerConfig = nextSwaggerDoc({
+// //   title: 'smembers API Documentation',
+// //   version: '1.0.0',
+// //   apiFolder: 'app/api',
+// // });
+// // const nextConfig = {
+// //   ...swaggerConfig,
+// // };
+
+// // async function loadSwaggerConfig() {
+// //   const nextSwagger = await import('next-swagger-doc');
+// //   return nextSwagger.default({
+// //     title: 'smembers API Documentation',
+// //     version: '1.0.0',
+// //     apiFolder: 'app/api',
+// //   });
+// // }
+
+// // const swaggerConfig = await loadSwaggerConfig();
+
+// // const nextConfig = {
+// //   ...swaggerConfig,
+// // };
+// // export default nextConfig;
+// import nextSwagger from 'next-swagger-doc';
+
+// const swaggerConfig = nextSwagger({
+//   title: 'smembers API Documentation',
+//   version: '1.0.0',
+//   apiFolder: 'app/api',
+// });
+
+// const nextConfig = {
+//   ...swaggerConfig,
+// };
+
+// module.exports = nextConfig;
